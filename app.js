192a8e005c2414eb252a6f1a79e848998c98e740
PrasanthJayaraman <prasanthj@outlook.com>
2019-01-21 21:31:16 +0530

    Bug fix in password change

--- app.js
+++ app.js
@@ -13,6 +13,33 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(cors());
+
+app.use(function (req, res, next) {
+  var oneof = false;
+  if (req.headers.origin) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    oneof = true;
+  }
+  if (req.headers['access-control-request-method']) {
+    res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+    oneof = true;
+  }
+  if (req.headers['access-control-request-headers']) {
+    res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+    oneof = true;
+  }
+  if (oneof) {
+    res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+  }
+
+  // intercept OPTIONS method
+  if (oneof && req.method == 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+});
+
 app.use(bodyParser.urlencoded({
   extended: false
 }))
