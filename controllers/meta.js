b2a6b0e8daec518ed3c31851b762382d029937e4
PrasanthJayaraman <prasanthj@outlook.com>
2019-03-16 10:31:48 +0530

    Fixed Dsr meta data on client

--- controllers/meta.js
+++ controllers/meta.js
@@ -23,26 +23,8 @@ exports.getMeta = function (req, res, next) {
             });
         } else {
             let data = meta.toJSON();
-            Config.findByUserId(user._id, function (err, config) {
-                if (err) {
-                    return res.status(500).send({
-                        message: "Error Saving Meta data"
-                    });
-                } else {
-                    console.log(config);
-                    let leads = _.map(config.leads, 'value');
-                    let sales = _.map(config.sales, 'value');
-                    for (let i = 0; i < meta.fields.length; i++) {
-                        if (meta.fields[i].key === 'leads') {
-                            meta.fields[i].value = leads;
-                        } else if (meta.fields[i].key === 'sales') {
-                            meta.fields[i].value = sales;
-                        }
-                    }
-                    data.fields = _.sortBy(meta.fields, 'priority');
-                    return res.status(200).send(data);
-                }
-            });
+            data.fields = _.sortBy(meta.fields, 'priority');
+            return res.status(200).send(data);
         }
     });
 }
